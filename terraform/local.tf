ba96296758ed006cab3cc0ad853ac0e691db7c6a
Shyam <shyam.rayaprolu@gmail.com>
2021-12-02 19:35:23 -0500

    added extra cidr for subnet

--- terraform/local.tf
+++ terraform/local.tf
@@ -13,8 +13,8 @@ locals {
   vpc_name                = "${local.stackname}-${local.subsystem_val}-vpc"
   vpc_cidr                = "10.0.0.0/16"
   availability_zones      = ["<aws-region>-1a","<aws-region>-1b","<aws-region>-1c"]
-  private_subnets         = ["10.0.1.0/24","10.0.2.0/24"]
-  public_subnets          = ["10.0.101.0/24","10.0.102.0/24"]
+  private_subnets         = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+  public_subnets          = ["10.0.101.0/24","10.0.102.0/24","10.0.103.0/24"]
   kubernetes_cluster_name = "${local.stackname}-${local.environment}-${local.subsystem_val}-ekscluster"
 
   #Userdata for nodes
